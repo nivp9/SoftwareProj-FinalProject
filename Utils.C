679ce9ee8adcd480664db54e224d61877d377635
nivp9 <hagainivp@mail.tau.ac.il>
2023-02-24 17:20:27 +0200

    added declarations

--- Utils.C
+++ Utils.C
@@ -6,6 +6,11 @@
             printf("An Error Has Occurred");    \
             exit(1);                           \
         }                                       \
+double ** multMatrix(double **A, double ** B, int An, int Am, int Bn, int Bm);
+double euclideanDist(double* a, double* b, int n);
+double W(double** A,int i, int j, int n);
+double** Transpose(double**A, int n, int m);
+void freeMatrix(double** A, int n, int m );
 
 
 int main(int argc, char *argv[] )
